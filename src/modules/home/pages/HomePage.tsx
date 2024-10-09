13b719a0d69d2b16423f60970b859f0096bc36c3
phamphuong <otcute03@gmail.com>
2024-10-09 15:26:44 +0700

    Create Home UI

--- src/modules/home/pages/HomePage.tsx
+++ src/modules/home/pages/HomePage.tsx
@@ -1,3 +1,19 @@
+import { Footer } from "antd/es/layout/layout";
+import Header from "components/Header";
+import BookNew from "components/Home/BookNew";
+import DeXuat from "components/Home/DeXuat";
+import Rank from "components/Home/Rank";
+import Slider from "components/Home/Slider";
+
 export default function HomePage() {
-  return <div>HomePage</div>
+  return (
+    <div>
+      <Header />
+      <Slider />
+      <BookNew />
+      <DeXuat />
+      <Rank />
+      <Footer />
+    </div>
+  );
 }
